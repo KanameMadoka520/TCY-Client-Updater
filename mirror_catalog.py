f30d882afdc579a9b9e90f77105d7c33ccf5872c
KanameMadoka520 <2441883200@qq.com>
2026-06-01 13:35:05 +0800

    Remove unavailable GitHub mirrors

--- mirror_catalog.py
+++ mirror_catalog.py
@@ -2,12 +2,9 @@ MIRROR_CATALOG = [
     {"label": "gh-proxy.org", "url": "https://gh-proxy.org/"},
     {"label": "ghproxy.cc", "url": "https://ghproxy.cc/"},
     {"label": "ghproxy.net", "url": "https://ghproxy.net/"},
-    {"label": "moeyy.xyz", "url": "https://github.moeyy.xyz/"},
-    {"label": "ghps.cc", "url": "https://ghps.cc/"},
     {"label": "gh.ddlc.top", "url": "https://gh.ddlc.top/"},
     {"label": "ghfast.top", "url": "https://ghfast.top/"},
     {"label": "cf.ghproxy.cc", "url": "https://cf.ghproxy.cc/"},
-    {"label": "gh.llkk.cc", "url": "https://gh.llkk.cc/"},
 ]
 
 
